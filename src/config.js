08ab5272be1b843bda88edae35a500c79a3abe8c
gvbvdxx <123641869+gvbvdxx@users.noreply.github.com>
2026-01-15 13:46:43 +0000

    Commit

--- src/config.js
+++ src/config.js
@@ -5,9 +5,16 @@ var configDirectory = path.join(__dirname, "../config/");
 var baseConfig = require("../config/relay.config.js");
 var config = { ...baseConfig };
 
-config.description = fs.readFileSync(
-  path.join(configDirectory, "description.txt"),
-  { encoding: "UTF-8" }
-);
+config.description = fs
+  .readFileSync(path.join(configDirectory, "description.txt"), {
+    encoding: "UTF-8",
+  })
+  .trim();
+
+config.name = fs
+  .readFileSync(path.join(configDirectory, "name.txt"), {
+    encoding: "UTF-8",
+  })
+  .trim();
 
 module.exports = config;
--- src/http/index.js
+++ src/http/index.js
@@ -8,6 +8,17 @@ function onHttpRequest(req, res) {
   var url = decodeURIComponent(req.url);
   var urlsplit = url.split("/");
 
+  if (urlsplit[1] == "status") {
+    res.end(
+      JSON.stringify({
+        status: "online",
+        name: config.name,
+        description: config.description,
+      })
+    );
+    return;
+  }
+
   serveStatic(req, res);
 }
 

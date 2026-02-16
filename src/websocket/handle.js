750443a3b27c1e10c5ef6029409c43cabacbfdb0
Gvbvdxx (Alt Account) <gvbvdxxalt2@outlook.com>
2026-02-16 17:59:40 +0000

    Commit

--- src/websocket/handle.js
+++ src/websocket/handle.js
@@ -4,6 +4,7 @@ var { UDPNetgame } = require("../netgame/");
 var HostDataChannel = require("../netgame/datach.js");
 var WSErrorCodes = require("./errors.js");
 var config = require("../config.js");
+var {setCorsHeaders} = require("./serve");
 var wss = new ws.WebSocketServer({
   noServer: true,
   ...config.WebsocketConfig,
